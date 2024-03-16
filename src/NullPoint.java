432fed6bffd574e375e46322e6186ce928b3a25b
Anas1997-star <144616315+Anas1997-star@users.noreply.github.com>
2024-03-16 14:18:29 +0530

    first commit

---
+++ src/NullPoint.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+
+public class NullPoint {
+public static void main(String[] args) {
+//	String s=null;//if s="value" then it prints value
+//	Optional<String> o=Optional.ofNullable(s);
+//	if(o.isPresent())
+//		System.out.println(o.get());
+//	else 
+//		System.out.println("value not present");
+//}
+ArrayList<String> al=new ArrayList<String>();
+al.add("shaikahmed");
+al.add("Ahmed");
+al.add("irfan");
+al.add("emran");
+Stream s=al.stream();//streamed APIs 
+s.filter(s1->((String) s1).endsWith("an")).sorted().forEach(n->System.out.println(n));
+
+}
+}
+
